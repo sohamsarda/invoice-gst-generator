9042f2af7ccaa5f16c422186143a48a54a97aedb
Soham Sarda <139840795+sohamsarda@users.noreply.github.com>
2025-07-21 19:14:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import InvoiceForm from './components/InvoiceForm';
 
+const MEASUREMENT_ID = "G-WRJKGSVRW8"; // Your Measurement ID
+
 class App extends Component {
+  componentDidMount() {
+    ReactGA.initialize(MEASUREMENT_ID);
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }
+
   render() {
   return (
     <div className="App d-flex flex-column align-items-center justify-content-center w-100">
